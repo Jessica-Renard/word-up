2f076c6fc2da56b263ace6518ab4fe24d2286c6f
Jessica-Renard <jessicalrenard@gmail.com>
2021-09-02 21:33:55 -0400

    upload

---
+++ letters.js
@@ -0,0 +1,279 @@
+// menu item
+const reset = document.getElementById("casesAll");
+const allAlpha = document.getElementById("alphabet");
+reset.addEventListener("click", updateAll);
+
+// alphabet within reader section
+// setting variables to id labels 
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const ten = document.getElementById("ten");
+const eleven = document.getElementById("eleven");
+const twelve = document.getElementById("twelve");
+const thirtn = document.getElementById("thirtn");
+const fourtn = document.getElementById("fourtn");
+const fiftn = document.getElementById("fiftn");
+const sixtn = document.getElementById("sixtn");
+const seventn = document.getElementById("seventn");
+const eightn = document.getElementById("eightn");
+const ninetn = document.getElementById("ninetn");
+const twenty = document.getElementById("twenty");
+const twone = document.getElementById("twone");
+const twtwo = document.getElementById("twtwo");
+const twthree = document.getElementById("twthree");
+const twfour = document.getElementById("twfour");
+const twfive = document.getElementById("twfive");
+const twsix = document.getElementById("twsix");
+
+// setting event listeners for clicks on divs by id
+one.addEventListener("click", updateA);
+two.addEventListener("click", updateB);
+three.addEventListener("click", updateC);
+four.addEventListener("click", updateD);
+five.addEventListener("click", updateE);
+six.addEventListener("click", updateF);
+seven.addEventListener("click", updateG);
+eight.addEventListener("click", updateH);
+nine.addEventListener("click", updateI);
+ten.addEventListener("click", updateJ);
+eleven.addEventListener("click", updateK);
+twelve.addEventListener("click", updateL);
+thirtn.addEventListener("click", updateM);
+fourtn.addEventListener("click", updateN);
+fiftn.addEventListener("click", updateO);
+sixtn.addEventListener("click", updateP);
+seventn.addEventListener("click", updateQ);
+eightn.addEventListener("click", updateR);
+ninetn.addEventListener("click", updateS);
+twenty.addEventListener("click", updateT);
+twone.addEventListener("click", updateU);
+twtwo.addEventListener("click", updateV);
+twthree.addEventListener("click", updateW);
+twfour.addEventListener("click", updateX);
+twfive.addEventListener("click", updateY);
+twsix.addEventListener("click", updateZ);
+
+// setting functions for text-transform specified by id variable
+// by removing or adding uppercase to classList, based on classList.contains
+function updateAll() {
+    if (allAlpha.classList.contains("uppercase")) {
+        allAlpha.classList.remove("uppercase");
+    } else {
+        allAlpha.classList.add("uppercase");
+    }
+}
+
+function updateA() {
+    if (one.classList.contains("uppercase")) {
+        one.classList.remove("uppercase");
+    } else {
+        one.classList.add("uppercase");
+    }
+}
+
+function updateB() {
+    if (two.classList.contains("uppercase")) {
+        two.classList.remove("uppercase");
+    } else {
+        two.classList.add("uppercase");
+    }
+}
+
+function updateC() {
+    if (three.classList.contains("uppercase")) {
+        three.classList.remove("uppercase");
+    } else {
+        three.classList.add("uppercase");
+    }
+}
+
+function updateD() {
+    if (four.classList.contains("uppercase")) {
+        four.classList.remove("uppercase");
+    } else {
+        four.classList.add("uppercase");
+    }
+}
+
+function updateE() {
+    if (five.classList.contains("uppercase")) {
+        five.classList.remove("uppercase");
+    } else {
+        five.classList.add("uppercase");
+    }
+}
+
+function updateF() {
+    if (six.classList.contains("uppercase")) {
+        six.classList.remove("uppercase");
+    } else {
+        six.classList.add("uppercase");
+    }
+}
+
+function updateG() {
+    if (seven.classList.contains("uppercase")) {
+        seven.classList.remove("uppercase");
+    } else {
+        seven.classList.add("uppercase");
+    }
+}
+
+function updateH() {
+    if (eight.classList.contains("uppercase")) {
+        eight.classList.remove("uppercase");
+    } else {
+        eight.classList.add("uppercase");
+    }
+}
+
+function updateI() {
+    if (nine.classList.contains("uppercase")) {
+        nine.classList.remove("uppercase");
+    } else {
+        nine.classList.add("uppercase");
+    }
+}
+
+function updateJ() {
+    if (ten.classList.contains("uppercase")) {
+        ten.classList.remove("uppercase");
+    } else {
+        ten.classList.add("uppercase");
+    }
+}
+
+function updateK() {
+    if (eleven.classList.contains("uppercase")) {
+        eleven.classList.remove("uppercase");
+    } else {
+        eleven.classList.add("uppercase");
+    }
+}
+
+function updateL() {
+    if (twelve.classList.contains("uppercase")) {
+        twelve.classList.remove("uppercase");
+    } else {
+        twelve.classList.add("uppercase");
+    }
+}
+
+function updateM() {
+    if (thirtn.classList.contains("uppercase")) {
+        thirtn.classList.remove("uppercase");
+    } else {
+        thirtn.classList.add("uppercase");
+    }
+}
+
+function updateN() {
+    if (fourtn.classList.contains("uppercase")) {
+        fourtn.classList.remove("uppercase");
+    } else {
+        fourtn.classList.add("uppercase");
+    }
+}
+
+function updateO() {
+    if (fiftn.classList.contains("uppercase")) {
+        fiftn.classList.remove("uppercase");
+    } else {
+        fiftn.classList.add("uppercase");
+    }
+}
+
+function updateP() {
+    if (sixtn.classList.contains("uppercase")) {
+        sixtn.classList.remove("uppercase");
+    } else {
+        sixtn.classList.add("uppercase");
+    }
+}
+
+function updateQ() {
+    if (seventn.classList.contains("uppercase")) {
+        seventn.classList.remove("uppercase");
+    } else {
+        seventn.classList.add("uppercase");
+    }
+}
+
+function updateR() {
+    if (eightn.classList.contains("uppercase")) {
+        eightn.classList.remove("uppercase");
+    } else {
+        eightn.classList.add("uppercase");
+    }
+}
+
+function updateS() {
+    if (ninetn.classList.contains("uppercase")) {
+        ninetn.classList.remove("uppercase");
+    } else {
+        ninetn.classList.add("uppercase");
+    }
+}
+
+function updateT() {
+    if (twenty.classList.contains("uppercase")) {
+        twenty.classList.remove("uppercase");
+    } else {
+        twenty.classList.add("uppercase");
+    }
+}
+
+function updateU() {
+    if (twone.classList.contains("uppercase")) {
+        twone.classList.remove("uppercase");
+    } else {
+        twone.classList.add("uppercase");
+    }
+}
+
+function updateV() {
+    if (twtwo.classList.contains("uppercase")) {
+        twtwo.classList.remove("uppercase");
+    } else {
+        twtwo.classList.add("uppercase");
+    }
+}
+
+function updateW() {
+    if (twthree.classList.contains("uppercase")) {
+        twthree.classList.remove("uppercase");
+    } else {
+        twthree.classList.add("uppercase");
+    }
+}
+
+function updateX() {
+    if (twfour.classList.contains("uppercase")) {
+        twfour.classList.remove("uppercase");
+    } else {
+        twfour.classList.add("uppercase");
+    }
+}
+
+function updateY() {
+    if (twfive.classList.contains("uppercase")) {
+        twfive.classList.remove("uppercase");
+    } else {
+        twfive.classList.add("uppercase");
+    }
+}
+
+function updateZ() {
+    if (twsix.classList.contains("uppercase")) {
+        twsix.classList.remove("uppercase");
+    } else {
+        twsix.classList.add("uppercase");
+    }
+}
---
+++ scripts.js
@@ -0,0 +1,228 @@
+/* "Word Up" early literacy exercises page (3-letter words' page)
+- based on 3 area clicks/touches so the common sounds are fully engaged across contexts for phonemic awareness
+- use quality asset files in .wav and .svg for clear engagement associations
+- follow functional paradigm in larger set of 4 and 5-letter exercises 
+- evaluate reload factors 
+- add reset */
+
+let l1, l2, l3, see, hear;
+
+/* nested by C/V/C group
+associate and incorporate .wav and .svg asset files by naming convention */
+const wordBankSets = [
+    [
+        "Dan", "Jan", "Pam", "Sam", "bad", "bam", "ban", "bat", "can", "cap", "car", "cat", "dad", "dam", "fan", "far", "fat", "gas", "had", "ham", "has", "hat",
+        "jab", "jam", "lad", "lap", "man", "map", "mat", "nap", "pan", "pat", "rad", "rap", "rat", "sad", "sap", "sat", "tan", "tap", "tar", "yam"
+    ],
+    [
+        "Ben", "Jed", "Jen", "Ted", "bed", "beg", "bet", "den", "fed", "gem", "get", "hem", "hen", "jet", "keg", "led", "leg", "let", "men", "met", "net", "peg", "pen",
+        "pep", "pet", "red", "set", "ten", "vet", "web", "wed", "wet", "yes", "yet", "zen"
+    ],
+    [
+        "Jim", "Kim", "Tim", "bib", "bid", "big", "bin", "bit", "did", "dig", "dim", "din", "dip", "fib", "fig", "fin", "fit", "gig", "hid", "him", "hip", "hit",
+        "jig", "kid", "kin", "kit", "lid", "lip", "lit", "nip", "pig", "pin", "pit", "rib", "rid", "rig", "rim", "rip", "sin", "sip", "sit", "tin", "tip", "wig",
+        "win", "wit", "yip", "zip"
+    ],
+    [
+        "Bob", "Jon", "Rob", "Ron", "Tom", "bog", "cob", "cod", "cog", "cop", "cot", "dog", "dot", "fog", "gob", "got", "hog", "hop", "hot", "job", "jog", "jot",
+        "log", "lot", "mob", "mom", "mop", "nod", "not", "pom", "pop", "pot", "rob", "rod", "rot", "sob", "sod", "top", "tot"
+    ],
+    [
+        "bud", "bug", "bum", "bun", "bus", "but", "cub", "cud", "cup", "cut", "dub", "dud", "dun", "fun", "gum", "gun", "gut", "hub", "hug", "hum", "hut", "jug",
+        "jut", "lug", "mud", "mug", "mum", "nub", "nun", "nut", "pug", "pun", "pup", "rub", "rug", "run", "rut", "sub", "sum", "sun", "tub", "tug"
+    ]
+];
+
+// each word group is nested 2 deep; saving each original data subset to variables for additional access dexterity.
+const aFiles = wordBankSets[0];
+const eFiles = wordBankSets[1];
+const iFiles = wordBankSets[2];
+const oFiles = wordBankSets[3];
+const uFiles = wordBankSets[4];
+
+/* words only and totally (from 3-letter bank) 
+combining in one variable can serve counts across exercises, word lists, practice options &or customizations */
+const words = [];
+for (let i = 0; i < wordBankSets.length; i++) {
+    for (let j = 0; j < wordBankSets[i].length; j++) {
+        words.push(wordBankSets[i][j]);
+    }
+}
+console.log(words.sort());
+
+// to derive sectional letter values for click/touch events to deliver text...
+// maintaining global variable contexts for 3-letter programmed exercises
+// check how this is altering arrays/pushing single letters into "words" array.
+const a = [],
+    e = [],
+    is = [],
+    o = [],
+    u = [];
+
+for (let word of words) {
+    if (word[1] === 'a') {
+        a.push(word);
+    } else if (word[1] === 'e') {
+        e.push(word);
+    } else if (word[1] === 'i') {
+        is.push(word);
+    } else if (word[1] === 'o') {
+        o.push(word);
+    } else if (word[1] === 'u') {
+        u.push(word);
+    }
+}
+
+// respective letter banks for "a", "e", & "u" word generations
+const aL1Pool = [],
+    aL3Pool = [],
+    eL1Pool = [],
+    eL3Pool = [],
+    uL1Pool = [],
+    uL3Pool = [];
+
+for (let unit of a) {
+    aL1Pool.push(unit[0]);
+    aL3Pool.push(unit[2]);
+}
+
+for (let unit of e) {
+    eL1Pool.push(unit[0]);
+    eL3Pool.push(unit[2]);
+}
+
+for (let unit of u) {
+    uL1Pool.push(unit[0]);
+    uL3Pool.push(unit[2]);
+}
+
+/* can also remove vowel and confirm modulo of 2 === 0 in order to:
+ xL1Pool.push() evens
+ xL3Pool.push() odds */
+// for "i" word generator
+const iLetters = [],
+    iL1Pool = [],
+    iL3Pool = [];
+
+for (let unit of is) {
+    for (let letter of unit) {
+        iLetters.push(letter);
+    }
+}
+const oneI = iLetters.join('');
+const noI = oneI.split('i').join('');
+
+for (i = 0; i < noI.length; i++) {
+    if (i % 2 === 0) {
+        iL1Pool.push(noI[i]); // evens are also 1st letters 
+    } else {
+        iL3Pool.push(noI[i]); // odds are the 3rd (made 2nd) letters
+    }
+}
+
+/* can increment by 2 for even/odd oL1/oL3, also among a string without the known vowel in the 3-letter pattern, thus evidencing reliable even/odd designations */
+// for "o" word generator
+const oLetters = [],
+    oL1Pool = [],
+    oL3Pool = [];
+
+for (let unit of o) {
+    for (let letter of unit) {
+        oLetters.push(letter);
+    }
+}
+const oneO = oLetters.join('');
+const noO = oneO.split('o').join('');
+
+for (i = 0; i < noO.length; i += 2) {
+    oL1Pool.push(noO[i]);
+}
+
+// here iterates by odd numbers
+for (i = 1; i < noO.length; i += 2) {
+    oL3Pool.push(noO[i]);
+}
+
+/* link #wordClick to click events, and each of the 3 digit spaces populate based on conditional pools, based on randomly generated short-vowel grouping */
+l1 = document.getElementById('l1');
+l2 = document.getElementById('l2');
+l3 = document.getElementById('l3');
+let letters3 = document.getElementById("vowelGroup");
+let l1Pool, l3Pool, ctA = 0,
+    ctE = 0,
+    ctI = 0,
+    ctO = 0,
+    ctU = 0;
+
+// subsequent click functions are nested among l2 click, so letter banks set specific to vowel not guaranteed word.
+l2.onclick = () => {
+
+    let vowelFirst = ['a', 'e', 'i', 'o', 'u'];
+    let randomIndex = Math.floor(Math.random() * 5);
+    let showVowel = vowelFirst[randomIndex];
+    l2.innerText = showVowel;
+
+    // I like that these letter pools automatically reflect occurrence frequencies
+    switch (showVowel) {
+        case "a": {
+            ctA++;
+            l1Pool = aL1Pool;
+            l3Pool = aL3Pool;
+            break;
+        }
+        case "e": {
+            ctE++;
+            l1Pool = eL1Pool;
+            l3Pool = eL3Pool;
+            break;
+        }
+        case "i": {
+            ctI++;
+            l1Pool = iL1Pool;
+            l3Pool = iL3Pool;
+            break;
+        }
+        case "o": {
+            ctO++;
+            l1Pool = oL1Pool;
+            l3Pool = oL3Pool;
+            break;
+        }
+        case "u": {
+            ctU++;
+            l1Pool = uL1Pool;
+            l3Pool = uL3Pool;
+            break;
+        }
+    }
+
+    l1.onclick = () => {
+        let randomIndex = Math.floor(Math.random() * l1Pool.length);
+        let showLetter = l1Pool[randomIndex];
+        l1.innerText = showLetter;
+    }
+
+    l3.onclick = () => {
+        let randomIndex = Math.floor(Math.random() * l3Pool.length);
+        let showLetter = l3Pool[randomIndex];
+        l3.innerText = showLetter;
+    }
+}
+
+let correct = 0, improve = [];
+// incorporate asset files for #show front-end (can also apply them to a rhyme-time game feature)
+// add onTouch, media queries and other accessibility
+
+// special logic for capital letter words featured in selections (ie rush L3 match)?
+// maybe reduce arrays based on letters positioned (for proper nouns)
+// use wordlists(.md) for UI and counts for usage features; add to resources(.md)
+// quick page layout and logic for toggling between uppercase and lowercase form of each alphabet letter.
+
+/* logics for longer word builds, wherein assign place values by its singly-determined 
+word click event, with its letter spaces' subsequent clicks displaying those subsequent phonemic groupings.*/
+
+/* To check the word, it's one click to gather from DOM contents
+and compare among array items, and it's one incrementor for Correct!
+
+*/
+
